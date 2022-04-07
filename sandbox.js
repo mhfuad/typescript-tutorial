4c41481b73d8087984aa41f996899bdd2722c952
mhfuad <mhfuad2@gmail.com>
2022-04-07 09:34:06 +0600

    array & object

--- sandbox.js
+++ sandbox.js
@@ -15,3 +15,38 @@ var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(7.5));
+// 4 object & array
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+//names.push(5) error
+//names[0]= 'toad'; error
+var numbers = [2, 3, 4, 5];
+numbers.push(2);
+//numbers.push("hi") //error
+//numbers[0] = "oi" //error
+var mixed = ["ken", 4, "bal", 3];
+mixed.push(2);
+mixed.push("hello");
+//----------------------------------------object 
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = "bal";
+//ninja.age = "oi" //error
+//ninja.skills = ["bal","fala"] //error
+//ninja = "stirng" error
+ninja = {
+    name: 'yoshi',
+    belt: "orange",
+    age: 40
+};
+//error
+// ninja = {
+//   name: 'yoshi',
+//   belt: "orange",
+//   age: 40,
+//   skills: ['oi', 1]
+// }
--- sandbox.ts
+++ sandbox.ts
@@ -23,3 +23,51 @@ const circ = (diameter: number) => {
 }
 
 console.log(circ(7.5));
+
+
+// 4 object & array
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+//names.push(5) error
+//names[0]= 'toad'; error
+
+
+let numbers = [2,3,4,5]
+numbers.push(2);
+//numbers.push("hi") //error
+//numbers[0] = "oi" //error
+
+
+let mixed = ["ken", 4, "bal", 3]
+mixed.push(2);
+mixed.push("hello")
+
+
+//----------------------------------------object 
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+}
+
+ninja.age = 40;
+ninja.name = "bal";
+//ninja.age = "oi" //error
+//ninja.skills = ["bal","fala"] //error
+
+//ninja = "stirng" error
+
+ninja = {
+  name: 'yoshi',
+  belt: "orange",
+  age: 40
+}
+
+//error
+// ninja = {
+//   name: 'yoshi',
+//   belt: "orange",
+//   age: 40,
+//   skills: ['oi', 1]
+// }

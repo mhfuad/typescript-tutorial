9f9baa93317b7ce2d133cc15b42e64d7df82541c
mhfuad <mhfuad2@gmail.com>
2022-04-07 14:33:38 +0600

    Interfaces

---
+++ public/app.js
@@ -0,0 +1,16 @@
+"use strict";
+const me = {
+    name: 'shaun',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log(`hello ${person.name}`);
+};
+greetPerson(me);
---
+++ src/app.ts
@@ -0,0 +1,24 @@
+interface Isperson {
+    name: string,
+    age: number,
+    speak(a: string ): void;
+    spend(a: number): number;
+}
+
+const me: Isperson = {
+    name: 'shaun',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number{
+        console.log('I spent', amount)
+        return amount
+    }
+}
+
+const greetPerson = (person: Isperson) => {
+    console.log(`hello ${person.name}`)
+}
+
+greetPerson(me);

ffcb7d1f103eaad773919eaf42f3532692659d1f
fuad <mhfuad2@gmail.com>
2022-04-09 19:38:04 +0600

    tuples

---
+++ public/tuples.js
@@ -0,0 +1,10 @@
+"use strict";
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, true];
+tup[0] = 'ken';
+tup[1] = 30;
+let student;
+student = ["shun-li", 23];

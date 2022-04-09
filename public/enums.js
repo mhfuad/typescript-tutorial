c2b5b02aa2f4d1a3e5966f660779d6b2f6a7c5e1
fuad <mhfuad2@gmail.com>
2022-04-09 19:19:38 +0600

    enums

---
+++ public/enums.js
@@ -0,0 +1,19 @@
+"use strict";
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const docOne = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { name: "name of bal" }
+};
+const docTwo = {
+    uid: 10,
+    resourceType: ResourceType.FILM,
+    data: { name: "yoshi" }
+};
---
+++ src/enums.ts
@@ -0,0 +1,19 @@
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
+
+interface Resource<T> {
+    uid: number;
+    resourceType: ResourceType;
+    data: T
+}
+
+const docOne: Resource<object> = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { name: "name of bal"}
+}
+
+const docTwo: Resource<object> = {
+    uid: 10,
+    resourceType: ResourceType.FILM,
+    data: {name:"yoshi"}
+}
